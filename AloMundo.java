d0ce162ff4d0af63953f25b4ca902babbeab53ca
mariaallyneluz <mariaallyneluz@gmail.com>
2018-06-15 19:36:20 -0300

    altercacoes feitas para o clone da pasta

--- AloMundo.java
+++ AloMundo.java
@@ -2,6 +2,8 @@ public class AloMundo {
 	public static void main (String[] args){
 		System.out.println("Alo Mundo");
 		System.out.println("Aloha");
+		System.out.println("Arquivo clonado");
+		
 	}
 
 }
